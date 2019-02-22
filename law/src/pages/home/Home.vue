334b5aad68b6acb21f9d78333f6c836c3aac2d64
Vue <1831860861@qq.com>
2019-02-22 19:45:00 +0800

    add axios

--- law/src/pages/home/Home.vue
+++ law/src/pages/home/Home.vue
@@ -13,6 +13,7 @@ import HomeHeader from 'common/Header'
 import HomePictures from 'common/Pictures'
 import HomeIcons from './components/iconSwiper'
 import HomeRecommend from './components/Recommend'
+import axios from 'axios'
 export default{
   name: 'Home',
   components: {
@@ -24,53 +25,26 @@ export default{
   data () {
     return {
       title: '法务通',
-      iconList: [{
-        'id': '0001',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-        'desc': '智能分析'
-      }, {
-        'id': '0002',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-        'desc': '智能分析'
-      }, {
-        'id': '0003',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-        'desc': '智能分析'
-      }, {
-        'id': '0004',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-        'desc': '智能分析'
-      }, {
-        'id': '0005',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-        'desc': '智能分析'
-      }, {
-        'id': '0006',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-        'desc': '智能分析'
-      }, {
-        'id': '0007',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-        'desc': '智能分析'
-      }, {
-        'id': '0008',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
-        'desc': '智能分析'
-      }],
-      recommendList: [{
-        'id': '0001',
-        'title': '政府强拆民房拒不承认 律师巧妙维权终获正义',
-        'desc': '王文梅(化名)等两人均系四川省眉山市富牛镇某村村民，1998年响应原眉山地区行政王文梅(化名)等两人均系四川省眉山市富牛镇某村村民，1998年响应原眉山地区行政'
-      }, {
-        'id': '0002',
-        'title': '政府强拆民房拒不承认 律师巧妙维权终获正义',
-        'desc': '王文梅(化名)等两人均系四川省眉山市富牛镇某村村民，1998年响应原眉山地区行政'
-      }, {
-        'id': '0003',
-        'title': '政府强拆民房拒不承认 律师巧妙维权终获正义',
-        'desc': '王文梅(化名)等两人均系四川省眉山市富牛镇某村村民，1998年响应原眉山地区行政'
-      }]
+      iconList: [],
+      recommendList: []
     }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json') // 通过创建static目录下的mock文件获取ajax数据,并根据请求的城市加载不同的home页面
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) { // 数据的获取
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+      }
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
   }
 }
 </script>
